a08c3f16a963960ac139b49e024bd6b9d76ca8b5
Asad-Ali-Developer <uhope1645@gmail.com>
2026-01-30 02:09:13 +0500

    Jan 30 Updates

--- config/index.ts
+++ config/index.ts
@@ -1,2 +1,2 @@
-// export const baseUrl = "https://fatima-marketing-backend-production.up.railway.app/api/v1";
-export const baseUrl = "http://localhost:8080/api/v1";
+export const baseUrl = "https://fatima-marketing-backend-production.up.railway.app/api/v1";
+// export const baseUrl = "http://localhost:8080/api/v1";
