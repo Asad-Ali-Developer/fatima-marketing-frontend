82b36c098a9042f2a461780791cf23882d13f0f4
Asad-Ali-Developer <uhope1645@gmail.com>
2026-01-31 23:23:46 +0500

    Added New backend

--- config/env.live.ts
+++ config/env.live.ts
@@ -1,4 +1,5 @@
-export const API_URL = 'https://fatima-marketing-backend-production.up.railway.app';
+// export const API_URL = 'https://fatima-marketing-backend-production.up.railway.app';
+export const API_URL = 'https://backend.fatimamarketing.xenvasol.com';
 
 export const productionEnvoirnmentConfig = {
   serverUrl: `${API_URL}/api/v1`,
