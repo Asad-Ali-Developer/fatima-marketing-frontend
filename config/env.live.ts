d8f9bd840d381122de1e6b5e7f9df2029cbcf349
Asad-Ali-Developer <uhope1645@gmail.com>
2026-08-15 15:58:33 +0500

    Updated Backend URL

--- config/env.live.ts
+++ config/env.live.ts
@@ -1,7 +1,5 @@
 // export const API_URL = 'https://fatima-marketing-backend-production.up.railway.app';
-export const API_URL =
-  "https://backend.crm.fatimamarketingofficial.com" ||
-  "https://fatima-marketing-backend-production.up.railway.app";
+export const API_URL = "https://backend.crm.fatimamarketingofficial.com";
 
 export const productionEnvoirnmentConfig = {
   serverUrl: `${API_URL}/api/v1`,
